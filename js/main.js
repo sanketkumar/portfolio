615468cdf7fe692710e8fbbf8c107c9fb6ce2ab2
sanketkumar <sanketskumar@yahoo.co.in>
2018-03-11 20:13:12 -0400

    Commenting out isScrolledIntoView

--- js/main.js
+++ js/main.js
@@ -173,13 +173,13 @@ var pieData1 = [{
        }
    });
 
-   function isScrolledIntoView(elem) {
-       var docViewTop = $(window).scrollTop();
-       var docViewBottom = docViewTop + $(window).height();
-       var elemTop = $(elem).offset().top;
-       var elemBottom = elemTop + $(elem).height();
-       return ((elemBottom >= docViewTop) && (elemTop <= docViewBottom) && (elemBottom <= docViewBottom) && (elemTop >= docViewTop));
-   }
+//    function isScrolledIntoView(elem) {
+//        var docViewTop = $(window).scrollTop();
+//        var docViewBottom = docViewTop + $(window).height();
+//        var elemTop = $(elem).offset().top;
+//        var elemBottom = elemTop + $(elem).height();
+//        return ((elemBottom >= docViewTop) && (elemTop <= docViewBottom) && (elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+//    }
 
    /* ------ End Document ----------
    ------------------------------------------ */
